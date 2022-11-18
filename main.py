b3a76663a260a3fdd975122b5529120bfb2bd098
KEVSER YOKUS <kevseryokuss@gmail.com>
2022-11-18 17:41:17 +0300

    addition, subtraction, multiplication, division in calculator

---
+++ main.py
@@ -0,0 +1,9 @@
+number1 = float(input("First Number  : "))
+number2 = float(input("Second Number : "))
+
+print("\n" * 3)
+print("collection result :" , number1 + number2)
+print("subtraction result :" , number1 - number2)
+print("multiplication result :" , number1 * number2)
+print("division result :" , number1 / number2)
+print("___________________________________")
